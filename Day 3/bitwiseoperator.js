821ed2df6b877f0031499820bd6b83e8c0542232
NIKHIL SRIVASTAV <69500241+Nikhil-Srivatsav@users.noreply.github.com>
2022-09-25 14:17:24 +0530

    Add files via upload

---
+++ Day 3/bitwiseoperator.js
@@ -0,0 +1,9 @@
+console.log(2<<1); //left shift operator
+
+console.log(4>>1); //right shift operator
+
+console.log(2 & 3);
+
+console.log(2 | 3);
+
+console.log(~3);
---
+++ Day 3/operators.js
@@ -0,0 +1,5 @@
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof `abcd`);
+console.log(typeof "abcd");
+console.log(typeof 'abcd');
