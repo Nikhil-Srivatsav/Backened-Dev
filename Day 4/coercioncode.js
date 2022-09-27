7d2156cfe948945b679a800699dddefa2db4ee99
NIKHIL SRIVASTAV <69500241+Nikhil-Srivatsav@users.noreply.github.com>
2022-09-27 15:52:23 +0530

    Update coercioncode.js

--- Day 4/coercioncode.js
+++ Day 4/coercioncode.js
@@ -1,3 +1,11 @@
+All the operations like 1+"1" or 1-"1" or "a"+2 are part of java script abstract operations 
+they are not executable operations and even we can not perform them but they are mentioned in documentation and 
+are written for internal understanding.
+
+what is coercion basically ?
+these are all basic internal operations that are meant to be performed for below operations and they arepredefined in java script documentations
+
+ We cannot implement them but understand how internally javascript works
 
 console.log(1+"a");
 
