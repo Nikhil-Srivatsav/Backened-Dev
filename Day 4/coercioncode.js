41d56621aba238a6ca4512e193287d46563ca25c
NIKHIL SRIVASTAV <69500241+Nikhil-Srivatsav@users.noreply.github.com>
2022-09-27 15:45:20 +0530

    Create coercioncode.js

---
+++ Day 4/coercioncode.js
@@ -0,0 +1,11 @@
+All the operations like 1+"1" or 1-"1" or "a"+2 are part of java script abstract operations 
+they are not executable operations and even we can not perform them but they are mentioned in documentation and 
+are written for internal understanding.
+
+console.log(1+"a");
+
+console.log(1-"a");
+
+console.log(1-"99");
+
+console.log("a"+1);
