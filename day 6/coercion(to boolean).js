a472f142ec12546cd2da8b5f0f0037dff9f611b9
NIKHIL SRIVASTAV <69500241+Nikhil-Srivatsav@users.noreply.github.com>
2022-09-30 23:25:44 +0530

    Update coercion(to boolean).js

--- day 6/coercion(to boolean).js
+++ day 6/coercion(to boolean).js
@@ -29,6 +29,5 @@ console.log({}==={}); // o/p would be false
 x={}; 
 
 // This is the concept of object value and we cam see x is pointing to a object now this time ojects are not compared but x is compared and x is pointing so true.
-
-console.log(x===x);
-
+Because we dont know what objects are being pointed out.
+console.log(x===x);   o/p would be true
