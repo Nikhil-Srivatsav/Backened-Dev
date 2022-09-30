ea0a6dfebd44621cc456be976438266638924a0a
NIKHIL SRIVASTAV <69500241+Nikhil-Srivatsav@users.noreply.github.com>
2022-09-30 22:58:41 +0530

    Update coercion(to boolean).js

--- day 6/coercion(to boolean).js
+++ day 6/coercion(to boolean).js
@@ -2,3 +2,7 @@
 
 '=='  is Abstract equality.
 '===' is Strict equality.
+
+
+if someone says that == checks value and === cjecks values and types just don't argue with them and ignore them.
+understand that he is a fool he has not spent time with javascript
