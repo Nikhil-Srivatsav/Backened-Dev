55c693e45827e08dea2a8b93095443d6d8749994
NIKHIL SRIVASTAV <69500241+Nikhil-Srivatsav@users.noreply.github.com>
2022-09-30 22:59:29 +0530

    Update coercion(to boolean).js

--- day 6/coercion(to boolean).js
+++ day 6/coercion(to boolean).js
@@ -6,3 +6,10 @@
 
 if someone says that == checks value and === cjecks values and types just don't argue with them and ignore them.
 understand that he is a fool he has not spent time with javascript.
+
+
+
+Now point is both checks value and perform further operations. Then whats the major difference 
+Note--> abstract equality checks and performs coercion.
+ 
+strict equality doesnt perform coercion it directly performs operations.
